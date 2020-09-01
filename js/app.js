7cb575c358f8725dd1d603dceb8532f54c5c4382
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-09-01 17:11:51 -0300

    Ajustado JS

--- js/app.js
+++ js/app.js
@@ -1,30 +1,32 @@
-const getPokemonUrl = id => ` https://pokeapi.co/api/v2/pokemon/${id}`
+(() => {
+    const getPokemonUrl = id => ` https://pokeapi.co/api/v2/pokemon/${id}`
 
-const fetchPokemon = () => {
-    const pokemonPromises = []
+    const fetchPokemon = () => {
+        const pokemonPromises = []
 
-    for (let i = 1; i <= 890; i++) {
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-    }
+        for (let i = 1; i <= 890; i++) {
+            pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+        }
 
-    Promise.all(pokemonPromises)
-        .then(pokemons => {
-            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+        Promise.all(pokemonPromises)
+            .then(pokemons => {
+                const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                    const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
-                accumulator += `
+                    accumulator += `
                 <li class = "card  ${types[0]}">
                     <img class = "card-image" alt= "${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
                     <h2 class = "card-tittle">${pokemon.id}.${pokemon.name.toUpperCase()}</h2>
                     <p class = "card-subtittle">${types.join(' | ')}</p>
                 </li>`
-                return accumulator
-            }, '')
+                    return accumulator
+                }, '')
 
-            const ul = document.querySelector('#pokedex')
+                const ul = document.querySelector('#pokedex')
 
-            ul.innerHTML = lisPokemons
-        })
-}
+                ul.innerHTML = lisPokemons
+            })
+    }
 
-fetchPokemon()
+    fetchPokemon()
+})()
